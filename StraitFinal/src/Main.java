dc6b6df2b47abae9d5b81117bfe7902916b2f439
Patrick Sampson <patrick.sampson19@outlook.com>
2022-04-19 21:50:39 -0300

    Completed Assignment 2

--- StraitFinal/src/Main.java
+++ StraitFinal/src/Main.java
@@ -3,14 +3,31 @@ public class Main {
 
         CharacterCreator creator = new CharacterCreator();
 
-        Player player = creator.generatePlayer("Maximus");
-        Enemy enemy = creator.generateEnemy("Brutus");
+        Player[] bracket1 = new Player[5];
+        Enemy[] bracket2 = new Enemy[5];
 
-        System.out.println(player.getName() + " starting health: " + player.getHealth());
-        System.out.println(enemy.getName() + " starting health: " + enemy.getHealth());
-        System.out.println("Let the battle begin");
+        bracket1[0] = creator.generatePlayer("Matt");
+        bracket1[1] = creator.generatePlayer("Liam");
+        bracket1[2] = creator.generatePlayer("Mitchell");
+        bracket1[3] = creator.generatePlayer("Brandon");
+        bracket1[4] = creator.generatePlayer("Alexander");
+        bracket2[0] = creator.generateEnemy("Blake");
+        bracket2[1] = creator.generateEnemy("Carmen");
+        bracket2[2] = creator.generateEnemy("Patrick");
+        bracket2[3] = creator.generateEnemy("ALyssa");
+        bracket2[4] = creator.generateEnemy("Lloyd");
+
+        for (int charcount = 0; charcount < bracket1.length; charcount++ ) {
+            System.out.println(bracket1[charcount].getName() + " starting health: " + bracket1[charcount].getHealth());
+        }
+        for (int charcount = 0; charcount < bracket1.length; charcount++ ) {
+            System.out.println(bracket2[charcount].getName() + " starting health: " + bracket2[charcount].getHealth());
+        }
+        System.out.println("Let the tournament begin");
 
         Battle battle = new Battle();
-        battle.runBattle(player, enemy);
+        //battle.bracketOne(bracket1);
+       // battle.bracketTwo(bracket2);
+        battle.tournamentBracket(bracket1, bracket2);
     }
 }
